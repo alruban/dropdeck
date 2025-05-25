ab8277ab0e6aeb4bdd0784ec98dcb2de03135bc4
Sam Clarke <alruban@protonmail.com>
2025-05-25 14:28:48 +0100

    work

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,11 +1,17 @@
 import {
   Page,
   Layout,
-  MediaCard,
-  BlockStack
+  BlockStack,
+  Text,
+  Card,
+  Box,
+  InlineGrid,
+  Button,
+  Link,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { useTranslation } from "../hooks/useTranslation";
+import banner from "../assets/banner_image.jpg";
 
 export default function Index() {
   const { t } = useTranslation();
@@ -14,28 +20,82 @@ export default function Index() {
     <Page>
       <TitleBar title={t("index.title")} />
       <Layout>
-        <BlockStack align="center">
-      <MediaCard
-        title="Getting Started"
-        primaryAction={{
-          content: 'Learn about getting started',
-          onAction: () => {},
-        }}
-        description="Discover how Shopify can power up your entrepreneurial journey."
-        popoverActions={[{content: 'Dismiss', onAction: () => {}}]}
-      >
-        <img
-          alt=""
-          width="100%"
-          height="100%"
-          style={{
-            objectFit: 'cover',
-            objectPosition: 'center',
-          }}
-          src="https://burst.shopifycdn.com/photos/business-woman-smiling-in-office.jpg?width=1850"
-        />
-      </MediaCard>
-      </BlockStack>
+        <Layout.Section>
+          <div
+            style={{
+              minHeight: "calc(100dvh - 57px - 56px)",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <BlockStack align="center">
+              <Card padding="0">
+                <InlineGrid columns={2}>
+                  <Box>
+                    <img
+                      alt=""
+                      width="100%"
+                      height="100%"
+                      style={{
+                        objectFit: "cover",
+                        objectPosition: "center",
+                      }}
+                      src={banner}
+                    />
+                  </Box>
+                  <Box padding="800">
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "14px",
+                        height: "100%",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Text as="h2" variant="headingLg" fontWeight="semibold">
+                        {t("index.card.title")}
+                      </Text>
+                      <Text as="p" variant="bodyLg" fontWeight="regular">
+                        {t("index.card.description")}
+                      </Text>
+
+                      <div
+                        style={{
+                          display: "grid",
+                          gridTemplateColumns: "1fr 1fr",
+                          gap: "14px",
+                          paddingTop: "20px",
+                        }}
+                      >
+                        <Link url="/app/route-quick-start">
+                          <Button
+                            variant="primary"
+                            size="large"
+                            fullWidth={true}
+                          >
+                            {t("index.card.primary_action")}
+                          </Button>
+                        </Link>
+                        <Link url="/app/route-orders">
+                          <Button
+                            variant="secondary"
+                            size="large"
+                            fullWidth={true}
+                          >
+                            {t("index.card.secondary_action")}
+                          </Button>
+                        </Link>
+                      </div>
+                    </div>
+                  </Box>
+                </InlineGrid>
+              </Card>
+            </BlockStack>
+          </div>
+        </Layout.Section>
       </Layout>
     </Page>
   );
