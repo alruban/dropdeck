0feb2bd3cd63b71e9c6318d64019961f00569726
Sam Clarke <alruban@protonmail.com>
2025-05-28 00:24:16 +0100

    work

---
+++ shared/mutations/update-product-sp-requirement.ts
@@ -0,0 +1,26 @@
+const UPDATE_PRODUCT_SP_REQUIREMENT_MUTATION = `#graphql
+  mutation UpdateProductSellingPlanRequirement($product: ProductUpdateInput!) {
+    productUpdate(product: $product) {
+      product {
+        id
+        requiresSellingPlan
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
+const updateProductSPRequirementVariables = (
+  id: string,
+  requiresSellingPlan: boolean
+) => ({
+  variables: {
+    id,
+    requiresSellingPlan,
+  },
+});
+
+export { UPDATE_PRODUCT_SP_REQUIREMENT_MUTATION, updateProductSPRequirementVariables };
