bf6187a5869b48c10f996052815ce556846b342d
Sam Clarke <alruban@protonmail.com>
2025-05-31 19:28:12 +0100

    work

--- app/routes/app.route-guide.tsx
+++ app/routes/app.route-guide.tsx
@@ -4,7 +4,6 @@ import {
   Card,
   Text,
   Box,
-  InlineGrid,
   BlockStack,
   Divider,
   List,
@@ -13,33 +12,75 @@ import {
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { useTranslation } from "../hooks/useTranslation";
-import bannerDesktop from "../assets/guide-landing-desktop.jpg";
-import bannerMobile from "../assets/guide-landing-mobile.jpg";
 import { useState } from "react";
 import { downloadFile } from "../utils/file-download";
 
 export default function Index() {
   const { t } = useTranslation();
 
-  // States
-  const [step, setStep] = useState(1);
+  // Pages
+  const [page, setPage] = useState(0);
+
+  function Contents({ hidden = true }) {
+    return (
+      <div hidden={hidden}>
+        <BlockStack gap="200">
+          <Text as="h3" variant="headingMd" fontWeight="medium">
+            {t("guide.card.pages.contents.title")}
+          </Text>
+          <Text as="p" variant="bodyMd">
+            {t("guide.card.pages.contents.description")}
+          </Text>
+          <Box paddingBlock="300">
+            <List type="number">
+              <List.Item>
+                <Button variant="plain" onClick={() => setPage(1)}>
+                  {t("guide.card.pages.contents.items.1")}
+                </Button>
+              </List.Item>
+              <List.Item>
+                <Button variant="plain" onClick={() => setPage(2)}>
+                  {t("guide.card.pages.contents.items.2")}
+                </Button>
+              </List.Item>
+              <List.Item>
+                <Button variant="plain" onClick={() => setPage(3)}>
+                  {t("guide.card.pages.contents.items.3")}
+                </Button>
+              </List.Item>
+              <List.Item>
+                <Button variant="plain" onClick={() => setPage(4)}>
+                  {t("guide.card.pages.contents.items.4")}
+                </Button>
+              </List.Item>
+              <List.Item>
+                <Button variant="plain" onClick={() => setPage(5)}>
+                  {t("guide.card.pages.contents.items.5")}
+                </Button>
+              </List.Item>
+            </List>
+          </Box>
+        </BlockStack>
+      </div>
+    )
+  }
 
   function Step1({ hidden = true }) {
     return (
       <div hidden={hidden}>
         <BlockStack gap="200">
           <Text as="h3" variant="headingMd" fontWeight="medium">
-            {t("guide.card.steps.1.title")}
+            {t("guide.card.pages.1.title")}
           </Text>
           <Text as="p" variant="bodyMd">
-            {t("guide.card.steps.1.description")}
+            {t("guide.card.pages.1.description")}
           </Text>
           <List>
-            <List.Item>{t("guide.card.steps.1.steps.1")}</List.Item>
-            <List.Item>{t("guide.card.steps.1.steps.2")}</List.Item>
+            <List.Item>{t("guide.card.pages.1.pages.1")}</List.Item>
+            <List.Item>{t("guide.card.pages.1.pages.2")}</List.Item>
           </List>
           <Text as="p" variant="bodyMd">
-            {t("guide.card.steps.1.conclusion")}
+            {t("guide.card.pages.1.conclusion")}
           </Text>
         </BlockStack>
       </div>
@@ -51,22 +92,22 @@ export default function Index() {
       <div hidden={hidden}>
         <BlockStack gap="200">
           <Text as="h3" variant="headingMd" fontWeight="medium">
-            {t("guide.card.steps.2.title")}
+            {t("guide.card.pages.2.title")}
           </Text>
           <Text as="p" variant="bodyMd">
-            {t("guide.card.steps.2.description")}
+            {t("guide.card.pages.2.description")}
           </Text>
           <Text as="h4" variant="headingSm" fontWeight="medium">
-            {t("guide.card.steps.2.choices.1.title")}
+            {t("guide.card.pages.2.choices.1.title")}
           </Text>
           <Text as="p" variant="bodyMd">
-            {t("guide.card.steps.2.choices.1.description")}
+            {t("guide.card.pages.2.choices.1.description")}
           </Text>
           <Text as="h4" variant="headingSm" fontWeight="medium">
-            {t("guide.card.steps.2.choices.2.title")}
+            {t("guide.card.pages.2.choices.2.title")}
           </Text>
           <Text as="p" variant="bodyMd">
-            {t("guide.card.steps.2.choices.2.description")}
+            {t("guide.card.pages.2.choices.2.description")}
           </Text>
         </BlockStack>
       </div>
@@ -78,42 +119,42 @@ export default function Index() {
       <div hidden={hidden}>
         <BlockStack gap="200">
           <Text as="h3" variant="headingMd" fontWeight="medium">
-            {t("guide.card.steps.3.title")}
+            {t("guide.card.pages.3.title")}
           </Text>
           <Text as="p" variant="bodyMd">
-            {t("guide.card.steps.3.description")}
+            {t("guide.card.pages.3.description")}
           </Text>
           <List>
             <List.Item>
               <Text as="h4" variant="headingSm" fontWeight="medium">
-                {t("guide.card.steps.3.snippets.1.title")}
+                {t("guide.card.pages.3.snippets.1.title")}
               </Text>
               <Text as="p" variant="bodyMd">
-                {t("guide.card.steps.3.snippets.1.description")}
+                {t("guide.card.pages.3.snippets.1.description")}
               </Text>
             </List.Item>
             <List.Item>
               <Text as="h4" variant="headingSm" fontWeight="medium">
-                {t("guide.card.steps.3.snippets.2.title")}
+                {t("guide.card.pages.3.snippets.2.title")}
               </Text>
               <Text as="p" variant="bodyMd">
-                {t("guide.card.steps.3.snippets.2.description")}
+                {t("guide.card.pages.3.snippets.2.description")}
               </Text>
             </List.Item>
             <List.Item>
               <Text as="h4" variant="headingSm" fontWeight="medium">
-                {t("guide.card.steps.3.snippets.3.title")}
+                {t("guide.card.pages.3.snippets.3.title")}
               </Text>
               <Text as="p" variant="bodyMd">
-                {t("guide.card.steps.3.snippets.3.description")}
+                {t("guide.card.pages.3.snippets.3.description")}
               </Text>
             </List.Item>
             <List.Item>
               <Text as="h4" variant="headingSm" fontWeight="medium">
-                {t("guide.card.steps.3.snippets.4.title")}
+                {t("guide.card.pages.3.snippets.4.title")}
               </Text>
               <Text as="p" variant="bodyMd">
-                {t("guide.card.steps.3.snippets.4.description")}
+                {t("guide.card.pages.3.snippets.4.description")}
               </Text>
             </List.Item>
           </List>
@@ -126,9 +167,49 @@ export default function Index() {
                 });
               }}
             >
-              {t("guide.card.steps.3.download")}
+              {t("guide.card.pages.3.download")}
             </Button>
           </Box>
+        </BlockStack>
+      </div>
+    )
+  }
+
+  function Step4({ hidden = true }) {
+    return (
+      <div hidden={hidden}>
+        <BlockStack gap="200">
+          <Text as="h3" variant="headingMd" fontWeight="medium">
+            {t("guide.card.pages.4.title")}
+          </Text>
+          <Text as="p" variant="bodyMd">
+            {t("guide.card.pages.4.description.1")}
+          </Text>
+          <Text as="p" variant="bodyMd">
+            {t("guide.card.pages.4.description.2")}
+          </Text>
+        </BlockStack>
+      </div>
+    )
+  }
+
+  function FrequentlyAskedQuestions({ hidden = true }) {
+    return (
+      <div hidden={hidden}>
+        <BlockStack gap="200">
+          <Text as="h3" variant="headingMd" fontWeight="medium">
+            {t("guide.card.pages.faqs.title")}
+          </Text>
+          <List>
+            <List.Item>
+              <Text as="h4" variant="headingSm" fontWeight="medium">
+                {t("guide.card.pages.faqs.questions.1.question")}
+              </Text>
+              <Text as="p" variant="bodyMd">
+                {t("guide.card.pages.faqs.questions.1.answer")}
+              </Text>
+            </List.Item>
+          </List>
         </BlockStack>
       </div>
     )
@@ -139,70 +220,80 @@ export default function Index() {
       <TitleBar title={t("guide.title")} />
       <Layout>
         <Layout.Section>
-          <Card padding="0">
-            <InlineGrid columns={{ xs: "1fr", md: "400px 1fr" }}>
-              <Box>
-                <picture>
-                  <source media="(max-width: 767px)" srcSet={bannerMobile} />
-                  <source media="(min-width: 768px)" srcSet={bannerDesktop} />
-                  <img
-                    src={bannerDesktop}
-                    alt=""
-                    width="100%"
-                    height="100%"
-                    style={{ objectFit: "cover", objectPosition: "center" }}
-                    loading="lazy"
-                  />
-                </picture>
-              </Box>
-              <div style={{
-                display: "flex",
-                flexDirection: "column",
-                padding: "36px",
-                height: "100%",
-              }}>
-                <BlockStack gap="200">
-                  <Text as="h2" variant="headingLg" fontWeight="medium">
-                    {t("guide.card.title")}
-                  </Text>
-                  <Text as="p" variant="bodyMd">
-                    {t("guide.card.description")}
-                  </Text>
-                </BlockStack>
-
-                <Box paddingBlock="400">
-                  <Divider />
-                </Box>
-
-                <Box minHeight="480px">
-                  <BlockStack gap="400">
-                    <Step1 hidden={step !== 1} />
-                    <Step2 hidden={step !== 2} />
-                    <Step3 hidden={step !== 3} />
-                  </BlockStack>
-                </Box>
-
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box maxWidth="600px">
+              <Card padding="0">
                 <div style={{
-                  marginTop: "auto",
                   display: "flex",
-                  justifyContent: "flex-end",
-                  alignItems: "center",
-                  paddingBlockStart: "36px",
+                  flexDirection: "column",
+                  padding: "36px",
+                  height: "100%",
                 }}>
-                  <Pagination
-                    hasPrevious={step > 1}
-                    onPrevious={() => {
-                      setStep(step - 1);
-                    }}
-                    hasNext={step < 3}
-                    onNext={() => {
-                      setStep(step + 1);
-                    }}
-                  />
+                  <BlockStack gap="200">
+                    <Text as="h2" variant="headingLg" fontWeight="medium">
+                      {t("guide.card.title")}
+                    </Text>
+                    <Text as="p" variant="bodyMd">
+                      {t("guide.card.description")}
+                    </Text>
+                  </BlockStack>
+
+                  <Box paddingBlock="400">
+                    <Divider />
+                  </Box>
+
+                  <Box minHeight="480px">
+                    <BlockStack gap="400">
+                      <Contents hidden={page !== 0} />
+                      <Step1 hidden={page !== 1} />
+                      <Step2 hidden={page !== 2} />
+                      <Step3 hidden={page !== 3} />
+                      <Step4 hidden={page !== 4} />
+                      <FrequentlyAskedQuestions hidden={page !== 5} />
+                    </BlockStack>
+                  </Box>
+
+                  <div style={{
+                    marginTop: "auto",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    paddingBlockStart: "36px",
+                  }}>
+                    <Box>
+                      <Button
+                        variant="secondary"
+                        fullWidth={false}
+                        onClick={() => {
+                          setPage(0);
+                        }}
+                      >
+                        {t("guide.card.return_to_contents")}
+                      </Button>
+                    </Box>
+
+                    <Pagination
+                      hasPrevious={page > 0}
+                      onPrevious={() => {
+                        setPage(page - 1);
+                      }}
+                      hasNext={page < 5}
+                      onNext={() => {
+                        setPage(page + 1);
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-            </InlineGrid>
-          </Card>
+              </Card>
+            </Box>
+          </div>
         </Layout.Section>
       </Layout>
     </Page>
