64238df8d79d77c098756cc55384c43f17942b98
Sam Clarke <alruban@protonmail.com>
2025-06-08 22:04:38 +0100

    fix: date issues

--- shared/tools/date-tools.ts
+++ shared/tools/date-tools.ts
@@ -5,6 +5,9 @@ export const createISOString = (date: string, hours?: number, minutes?: number):
   // Set the hours and minutes
   if (hours && minutes) {
     dateObj.setHours(hours, minutes, 0, 0);
+  } else {
+    // Set to noon to avoid timezone issues
+    dateObj.setHours(12, 0, 0, 0);
   }
 
   // Return ISO string
